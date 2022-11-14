035710ec59d52116b84b4be0b77a7de639d4508a
Jeevika <112706346+Jeevi2001@users.noreply.github.com>
2022-11-14 22:30:13 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 2/app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/home1')
+def home1():
+    return render_template("home1.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/features')
+def features():
+    return render_template("features.html")
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=8080,debug=True)
